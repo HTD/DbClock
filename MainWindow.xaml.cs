a5f8ab242f5452f6e995df56bc27af173085298e
Adam Łyskawa <adam@codedog.pl>
2020-12-17 13:57:24 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace DbClock {
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        
+        public MainWindow() {
+            InitializeComponent();
+            var movement = new Movement();
+            movement.Tick += (s, e) => Clock.Time = movement.Interpolated;
+        }
+
+        private void Menu_Exit(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+    }
+
+}
---
+++ Movement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace DbClock {
+    
+    class Movement {
+
+        public event EventHandler Tick;
+
+        public DateTime Time { get; private set; }
+
+        public DateTime Interpolated { get; private set; }
+
+        public TimeSpan Offset { get; private set; }
+
+        public TimeSpan TimerResolution { get; set; } = TimeSpan.FromMilliseconds(16.7);
+
+        public TimeSpan TimerUpdateTime { get; set; } = TimeSpan.FromSeconds(0.1);
+
+        public Func<DateTime> GetTime { get; set; } = () => DateTime.Now;
+
+        public Movement(Dispatcher dispatcher = null, DispatcherPriority priority = DispatcherPriority.Render) {
+            if (dispatcher is null) dispatcher = Dispatcher.CurrentDispatcher;
+            new DispatcherTimer(TimerResolution, priority, OnTimer, dispatcher);
+        }
+        
+        void OnTimer(object sender, EventArgs e) {
+            var timer = sender as DispatcherTimer;
+            SetTime(GetTime());
+            if (timer.Interval == TimerResolution) {
+                if (Offset < TimerResolution) timer.Interval = TimerUpdateTime;
+            }
+            else if (timer.Interval > TimerResolution) Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        void SetTime(DateTime time) {
+            var secondStartTick = (time.Ticks / TimeSpan.TicksPerSecond) * TimeSpan.TicksPerSecond;
+            Offset = TimeSpan.FromTicks(time.Ticks - secondStartTick);
+            Interpolated = time.Add(-Offset);
+            Time = time;
+        }
+
+    }
+
+}
